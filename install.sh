125ae7bed48be5cc3fe04adfc53db9eceaac6bd8
Annie <tgxtvf333@mozmail.com>
2026-06-09 16:03:57 +0300

    Switch to apk/nftables

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 # Update package
-opkg update
+apk update
 
 #install packages
-opkg install iptables iptables-mod-nat-extra redsocks
+apk add nftables redsocks
 
 #Then run this line
-service redsocks stop && mv /etc/redsocks.conf /etc/redsocks.conf.bkp && cd /etc && wget https://github.com/emonbhuiyan/Redsocks-OpenWRT/raw/main/redsocks.conf && mv /etc/init.d/redsocks /etc/init.d/redsocks.bkp && cd /etc/init.d && wget https://github.com/emonbhuiyan/Redsocks-OpenWRT/raw/main/redsocks && chmod +x /etc/init.d/redsocks
+service redsocks stop && mv /etc/redsocks.conf /etc/redsocks.conf.bkp && cd /etc && wget https://github.com/Gojikovi/Redsocks-OpenWRT/raw/main/redsocks.conf && mv /etc/init.d/redsocks /etc/init.d/redsocks.bkp && cd /etc/init.d && wget https://github.com/Gojikovi/Redsocks-OpenWRT/raw/main/redsocks && chmod +x /etc/init.d/redsocks
 
-echo -e "Thanks for installing. Follow me for more updates. https://fb.me/emoncontact"
+echo -e "Thanks for installing."
